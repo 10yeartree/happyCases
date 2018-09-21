85f038223989e10804ac3530936cadddfa6a6913
bfx <abc@kkkfffccc.com>
2018-09-21 22:32:20 +0800

    optimize code

--- 002_time_format_in_ruby/code.rb
+++ 002_time_format_in_ruby/code.rb
@@ -5,6 +5,8 @@ require 'date'
 
 (0...24).each do |hour|
 	timeStr_24Hour = ("%02d" % hour) + ":00:00"
+	timeStr_24Hour_AM_PM = DateTime.strptime(timeStr_24Hour, "%H:%M:%S").strftime("%H:%M:%S %p")
 	timeStr_12Hour = DateTime.strptime(timeStr_24Hour, "%H:%M:%S").strftime("%l:%M:%S %p")
-	puts "#{timeStr_24Hour} => #{timeStr_12Hour}"
+
+	puts "#{timeStr_24Hour} => #{timeStr_24Hour_AM_PM} => #{timeStr_12Hour}"
 end   
